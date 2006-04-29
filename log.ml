e2c7c8e8544274ccfa8a129d4190f2e35b4dcd5a
Eric Cooper <ecc@cmu.edu>
2006-04-29 00:28:49 +0000

    don't fail if daemon can't connect to syslogd

--- log.ml
+++ log.ml
@@ -2,13 +2,17 @@ open Printf
 
 let printer = ref (fun _ -> prerr_endline)
 
-let use_syslog () =
-  let prog = Filename.basename Sys.argv.(0) in
-  let log = Syslog.openlog ~facility: `LOG_DAEMON prog in
-  printer := Syslog.syslog log
-
 let message level fmt = kprintf (fun str -> !printer level str) fmt
 
 let error_message fmt = message `LOG_ERR fmt
 let info_message fmt = message `LOG_INFO fmt
 let debug_message fmt = message `LOG_DEBUG fmt
+
+let use_syslog () =
+  let prog = Filename.basename Sys.argv.(0) in
+  try
+    let log = Syslog.openlog ~facility: `LOG_DAEMON prog in
+    printer := Syslog.syslog log
+  with _ ->
+    error_message "Cannot connect to system logger";
+    printer := (fun _ -> ignore)
