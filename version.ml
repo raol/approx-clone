f71ed400a715dc37fd42abcf857ee1a75b15391d
Eric Cooper <ecc@cmu.edu>
2008-10-24 17:38:58 -0400

    remove upstream version.ml so git-buildpackage won't complain

--- version.ml
+++
@@ -1,6 +0,0 @@
-(* approx: proxy server for Debian archive files
-   Copyright (C) 2008  Eric C. Cooper <ecc@cmu.edu>
-   Released under the GNU General Public License *)
-
-let name = "approx"
-let number = "3.4"
