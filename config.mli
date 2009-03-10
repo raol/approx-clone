842b2f0251007698e04e5fd210db0497cafc98b2
Eric Cooper <ecc@cmu.edu>
2009-03-10 13:04:29 -0400

    remove Version module
    move version from Version to Config module
    move split_cache_path from Url to Config module
    add shorten function to Config module

--- config.mli
+++ config.mli
@@ -2,10 +2,22 @@
    Copyright (C) 2009  Eric C. Cooper <ecc@cmu.edu>
    Released under the GNU General Public License *)
 
+val version : string
 val config_file : string
 val cache_dir : string
 
-val max_rate : string  (* bytes/second with optional K, M, or G suffix *)
+(* Extract the distribution and relative filename
+   from the absolute pathname of a file in the cache.
+   Example: split_pathname "/var/cache/approx/debian/pool/main/..."
+   returns ("debian", "pool/main/...") *)
+
+val split_cache_path : string -> string * string
+
+(* Remove cache directory prefix from a pathname, if present *)
+
+val shorten : string -> string
+
+val max_rate : string (* bytes/second with optional K, M, or G suffix *)
 val max_redirects : int
 
 val user : string
@@ -14,7 +26,7 @@ val syslog : string
 
 val pdiffs : bool
 val offline : bool
-val max_wait : int     (* seconds *)
+val max_wait : int (* seconds *)
 
 val verbose : bool
 val debug : bool
--- version.mli
+++
@@ -1,6 +0,0 @@
-(* approx: proxy server for Debian archive files
-   Copyright (C) 2006  Eric C. Cooper <ecc@cmu.edu>
-   Released under the GNU General Public License *)
-
-val name : string
-val number : string
