5141c652b91d6a84e362a781d37dc9fd88b13854
Eric Cooper <ecc@cmu.edu>
2011-03-09 13:03:57 -0500

    increment version number

--- config.ml
+++ config.ml
@@ -1,11 +1,11 @@
 (* approx: proxy server for Debian archive files
-   Copyright (C) 2010  Eric C. Cooper <ecc@cmu.edu>
+   Copyright (C) 2011  Eric C. Cooper <ecc@cmu.edu>
    Released under the GNU General Public License *)
 
 open Config_file
 open Util
 
-let version = "4.5"
+let version = "4.6"
 
 let default_config = "/etc/approx/approx.conf"
 
