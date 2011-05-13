2fa312d33a93c35fa7f141a315250269ad908797
Eric Cooper <ecc@cmu.edu>
2011-05-13 10:20:56 -0400

    remove unused network.mli file

--- network.mli
+++
@@ -1,7 +0,0 @@
-(* approx: proxy server for Debian archive files
-   Copyright (C) 2008  Eric C. Cooper <ecc@cmu.edu>
-   Released under the GNU General Public License *)
-
-external interface_address : string -> Unix.inet_addr = "interface_address"
-
-external set_ipv6_only : Unix.file_descr -> bool -> unit = "set_ipv6_only"
